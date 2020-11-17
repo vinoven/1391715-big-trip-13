28951d24bb2ab9ddbc123f36bb3462eb0fb298d5
vinoven <mikhailsechin@gmail.com>
2020-11-18 01:06:00 +0300

    Настраивает ESLint и webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,9 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    watchContentBase: true
+  }
 };
