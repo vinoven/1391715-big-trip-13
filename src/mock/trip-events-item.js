bd40620a8459c700e13dbaf43799841de10d61a2
vinoven <mikhailsechin@gmail.com>
2020-11-24 01:51:32 +0300

    Завершает описание структуры "Точка маршрута"

--- src/mock/trip-events-item.js
+++ src/mock/trip-events-item.js
@@ -1,24 +1,4 @@
-// Точка маршрута
-
-// Тип точки маршрута
-// Пункт назначения
-// Описание
-// Фотографии
-// Дата и время начала события
-// Дата и время окончания события
-// Стоимость
-// Дополнительные опции
-// Кнопка favourite
-
-// Исходные данные
-
 import dayjs from "dayjs";
-
-const EVENTS_TYPE = [`Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`, `Check-in`, `Sightseeng`, `Restaurant`];
-const DESCRIPTION_TEXT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus. `;
-const PLACES_OF_DESTINATION = [`Amsterdam`, `Geneva`, `Chamonix`];
-const SENTENCES_MIN_AMOUNT = 1;
-const SENTENCES_MAX_AMOUNT = 5;
 
 // Служебные функции
 const getRandomInteger = (a = 0, b = 1) => {
@@ -37,20 +17,32 @@ const shuffleArray = (arr) => {
   return shuffledArray;
 };
 
+const getRandomTimeGap = () => {
+  return getRandomInteger(30, 360);
+};
+
 // Функции по генерации данных
 
-const generateType = (availableTypes) => {
-  const randomIndex = getRandomInteger(0, availableTypes.length - 1);
-  return availableTypes[randomIndex];
+const generateType = () => {
+  const EVENTS_TYPE = [`Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`, `Check-in`, `Sightseeng`, `Restaurant`];
+
+  const randomIndex = getRandomInteger(0, EVENTS_TYPE.length - 1);
+  return EVENTS_TYPE[randomIndex];
 };
 
-const generateDestination = (places) => {
-  const randomIndex = getRandomInteger(0, places.length - 1);
-  return places[randomIndex];
+const generateDestination = () => {
+  const PLACES_OF_DESTINATION = [`Amsterdam`, `Geneva`, `Chamonix`];
+
+  const randomIndex = getRandomInteger(0, PLACES_OF_DESTINATION.length - 1);
+  return PLACES_OF_DESTINATION[randomIndex];
 };
 
-const generateDescription = (text) => {
-  const descriptionSentences = text.split(`. `);
+const generateDescription = () => {
+  const DESCRIPTION_TEXT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus. `;
+  const SENTENCES_MIN_AMOUNT = 1;
+  const SENTENCES_MAX_AMOUNT = 5;
+
+  const descriptionSentences = DESCRIPTION_TEXT.split(`. `);
   const amountOfSentences = getRandomInteger(SENTENCES_MIN_AMOUNT, SENTENCES_MAX_AMOUNT);
   const description = shuffleArray(descriptionSentences).slice(0, amountOfSentences).join(`. `) + `.`;
   return description;
@@ -67,19 +59,57 @@ const generatePhotos = () => {
   return photosArray;
 };
 
+const generateEventCost = () => {
+  const cost = getRandomInteger(100, 5000);
+  return cost;
+};
+
+const generateOffers = () => {
+  const offers = [`Add luggage`, `Switch to comfort`, `Add meal`, `Chose seats`, `Travel by train`];
+  const numberOfOffers = getRandomInteger(0, offers.length - 1);
+  const generatedOffers = shuffleArray(offers).slice(0, numberOfOffers);
+  return generatedOffers;
+};
+
+const randomizeFavouriteFlag = () => {
+  return Boolean(getRandomInteger(0, 1));
+};
+
+const getEventStartTime = () => {
+  const gap = 24;
+  const gapInHours = getRandomInteger(-gap, gap);
+  return dayjs().add(gapInHours, `hour`).toDate();
+};
+
+const getEventEndTime = (startTime, duration) => {
+  return dayjs(startTime).add(duration, `minute`).toDate();
+};
+
 // Генерация точки маршрута
 
 export const generateTripEventsItem = () => {
+
+  const type = generateType();
+  const destination = generateDestination();
+  const description = generateDescription();
+  const photos = generatePhotos();
+  const eventStartTime = getEventStartTime();
+  const duration = getRandomTimeGap();
+  const eventEndTime = getEventEndTime(eventStartTime, duration);
+  const cost = generateEventCost();
+  const offers = generateOffers();
+  const isFavourite = randomizeFavouriteFlag();
+
   return {
-    type: generateType(EVENTS_TYPE),
-    destination: generateDestination(PLACES_OF_DESTINATION),
-    description: generateDescription(DESCRIPTION_TEXT),
-    photos: generatePhotos(),
-    startDateTime: null,
-    endDateTime: null,
-    cost: null,
-    offers: null,
-    isFavourite: false
+    type,
+    destination,
+    description,
+    photos,
+    eventStartTime,
+    eventEndTime,
+    cost,
+    offers,
+    isFavourite
   };
 };
 
