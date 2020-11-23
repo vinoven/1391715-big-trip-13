df48e79cd664ef27eabdec3ebbc5cc8580328215
vinoven <mikhailsechin@gmail.com>
2020-11-23 19:46:38 +0300

    Подключает dayjs

--- src/mock/trip-events-item.js
+++ src/mock/trip-events-item.js
@@ -12,6 +12,8 @@
 
 // Исходные данные
 
+import dayjs from "dayjs";
+
 const EVENTS_TYPE = [`Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`, `Check-in`, `Sightseeng`, `Restaurant`];
 const DESCRIPTION_TEXT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus. `;
 const PLACES_OF_DESTINATION = [`Amsterdam`, `Geneva`, `Chamonix`];
